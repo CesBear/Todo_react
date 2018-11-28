5176cb0a084e15b65c4b422f8e1231f4b5968056
Cesar Ramirez <carr.gdl1@gmail.com>
2018-11-28 15:35:26 -0600

    Boiler Plate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import '../App.css';
+import BrowserRouter from "react-router-dom/es/BrowserRouter";
+import Route from "react-router-dom/es/Route";
 
 class App extends Component {
   render() {
     return (
       <div>
-        Test
+          <BrowserRouter>
+            <div>
+                <Route exact path="/" />
+            </div>
+          </BrowserRouter>
       </div>
     );
   }
